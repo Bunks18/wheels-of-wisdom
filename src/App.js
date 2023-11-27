54e3003206319b5d3ee30483d93a392e87a48765
bunks18 <wbunker11@gmail.com>
2023-11-26 23:17:35 -0700

    feat: fix homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./Hero";
+import { IoIosPeople } from "react-icons/io";
+import { FaHandsHelping } from "react-icons/fa";
+import { PiPersonSimpleBike } from "react-icons/pi";
+import Logo from "./Logo";
+const Header = () => {
+  return (
+    <header className="bg-white shadow-md py-4">
+      <div className="container mx-auto flex flex-col md:flex-row md:justify-between md:items-center">
+        {/* <div className="font-bold text-gray-800 mb-2 md:mb-0"> */}
+        {/* <Logo className="h-1" /> */}
+        {/* </div> */}
+        {/* <Logo /> */}
+        <div className="flex space-x-4 mt-4 md:mt-0">
+          <a href="#" className="text-gray-600 hover:text-gray-800">
+            Home
+          </a>
+          <a href="#" className="text-gray-600 hover:text-gray-800">
+            Features
+          </a>
+          <a href="#" className="text-gray-600 hover:text-gray-800">
+            Contact
+          </a>
+        </div>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-full mt-4 md:mt-0 hover:bg-blue-600">
+          Get Started
+        </button>
+      </div>
+    </header>
+  );
+};
+
+const HeroSection = () => {
+  return (
+    <section className="container mx-auto py-16 text-left  md:flex md:justify-between md:items-center">
+      <div className="mb-8 md:w-1/2 md:pr-8">
+        <h1 className="text-4xl md:text-5xl font-extrabold text-gray-800 mb-4">
+          Cycling Wisdom, Building Connections
+        </h1>
+        <p className="text-gray-600">
+          Join Wheels of Wisdom, where younger generations pedal the path of
+          companionship for older members in the serene embrace of nature.
+          Experience the joy of shared journeys, building connections that
+          transcend generations.
+        </p>
+        <button className="bg-blue-500 text-white px-6 py-3 rounded-full mt-4 hover:bg-blue-600 transition duration-300">
+          Get Involved
+        </button>
+      </div>
+      <div className="md:w-1/2">
+        <Hero />{" "}
+      </div>
+    </section>
+  );
+};
+const FeatureSection = () => {
+  return (
+    <section className="bg-gray-100 py-16">
+      <div className="container mx-auto text-center mb-12">
+        <h2 className="text-3xl font-extrabold text-gray-800 mb-4">
+          How it Works
+        </h2>
+        <p className="text-gray-600">
+          Explore the features that outline how Wheels of Wisdom brings
+          generations together in the great outdoors.
+        </p>
+      </div>
+      <div className="container mx-auto flex flex-col md:flex-row md:justify-center md:items-center">
+        <div className="w-full md:w-1/3 text-left bg-white p-8 rounded-lg shadow-lg mb-8 md:mb-0 md:mr-8">
+          <div className="mb-4">
+            <div className="h-10">
+              <IoIosPeople className="text-5xl" />
+            </div>
+          </div>
+          <h3 className="text-xl font-semibold mb-2">
+            Assisted Living Partnerships
+          </h3>
+          <p className="text-gray-600">
+            We collaborate with local assisted living homes, fostering a sense
+            of community and well-being for their residents.
+          </p>
+        </div>
+        <div className="w-full md:w-1/3 text-left bg-white p-8 rounded-lg shadow-lg mb-8 md:mb-0 md:mx-4">
+          <div className="mb-4">
+            <FaHandsHelping className="text-5xl" />
+          </div>
+          <h3 className="text-xl font-semibold mb-2">Volunteer Engagement</h3>
+          <p className="text-gray-600">
+            We engage local volunteers with a passion for cycling and a
+            commitment to building connections with older community members.
+          </p>
+        </div>
+        <div className="w-full md:w-1/3 text-left bg-white p-8 rounded-lg shadow-lg md:ml-8">
+          <div className="mb-4">
+            <PiPersonSimpleBike class="text-5xl" />
+          </div>
+          <h3 className="text-xl font-semibold mb-2">Outdoor Biking</h3>
+          <p className="text-gray-600">
+            We match volunteers and assisted living residents, creating
+            opportunities for shared biking experiences in the outdoors.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const InfoSection = ({ title, text, imagePath, reverse }) => {
+  return (
+    <section className="bg-gray-100 py-16">
+      <div className="container mx-auto flex flex-col md:flex-row md:items-center">
+        <div className={`md:w-1/2 ${reverse ? "md:order-2" : ""}`}>
+          <Hero />
+        </div>
+        <div className={`md:w-1/2 md:pl-8 ${reverse ? "md:pr-8" : ""}`}>
+          <h2 className="text-3xl font-extrabold text-gray-800 mb-4">
+            {title}
+          </h2>
+          <p className="text-gray-600 mb-8">{text}</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const CTASection = () => {
+  return (
+    <section className="bg-blue-500 text-white py-16">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-extrabold mb-4">Ready to Get Started?</h2>
+        <p className="text-lg text-gray-200 mb-8">
+          Join us today and experience the clean revolution.
+        </p>
+        <button className="bg-white text-blue-500 px-6 py-3 rounded-full font-semibold hover:bg-gray-200 hover:text-blue-500 transition duration-300">
+          Sign Up Now
+        </button>
+      </div>
+    </section>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-8">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl font-bold">Clean Logo</div>
+        <div className="flex space-x-4">
+          <a href="#" className="hover:text-gray-300">
+            Home
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Features
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Contact
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <HeroSection />
+      <FeatureSection />
+      {/* <InfoSection
+        title="Welcome to the Clean World"
+        text="Discover the simplicity and elegance of our products and services. Join us in creating a clean and beautiful future."
+      /> */}
+      <CTASection />
+      <Footer />
     </div>
   );
 }
---
+++ src/Logo.js
@@ -0,0 +1,134 @@
+import * as React from "react";
+const Logo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={100}
+    height={100}
+    {...props}
+  >
+    <rect
+      width={100}
+      height={100}
+      x={-640}
+      y={-512}
+      fill="none"
+      paintOrder="stroke"
+      rx={0}
+      ry={0}
+      style={{
+        stroke: "none",
+        strokeWidth: 0,
+        strokeDasharray: "none",
+        strokeLinecap: "butt",
+        strokeDashoffset: 0,
+        strokeLinejoin: "miter",
+        strokeMiterlimit: 4,
+        fill: "#fff",
+        fillOpacity: 0,
+        fillRule: "nonzero",
+        opacity: 1,
+      }}
+      transform="translate(640 512)"
+    />
+    <g paintOrder="stroke">
+      <path
+        d="M0 0a4.5 4.5 0 0 1-4.5 4.5h-56.623a4.5 4.5 0 0 1 0-9H-4.5A4.5 4.5 0 0 1 0 0"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeDashoffset: 0,
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 4,
+          fill: "#000",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(3.30882 0 0 -3.30882 380.782 424.303)"
+      />
+      <path
+        d="M0 0h56.623a4.5 4.5 0 0 1 0 9H0a4.5 4.5 0 0 1 0-9"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeDashoffset: 0,
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 4,
+          fill: "#000",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(3.30882 0 0 -3.30882 241.338 377.178)"
+      />
+      <path
+        d="M0 0h56.623a4.5 4.5 0 0 1 0 9H0a4.5 4.5 0 0 1 0-9"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeDashoffset: 0,
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 4,
+          fill: "#000",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(3.30882 0 0 -3.30882 304.138 315.163)"
+      />
+      <path
+        d="M0 0c-26.822 0-48.644 21.821-48.644 48.644 0 26.821 21.822 48.642 48.644 48.642 5.2 0 10.208-.829 14.91-2.347L-2.302 50.261a4.501 4.501 0 0 1 8.399-3.236l17.092 44.369c15.149-8.251 25.454-24.32 25.454-42.75C48.643 21.821 26.821 0 0 0m-82.316 48.644C-82.316 21.821-104.138 0-130.959 0c-26.822 0-48.644 21.821-48.644 48.644 0 26.821 21.822 48.642 48.644 48.642a48.55 48.55 0 0 0 12.353-1.594l-17.501-45.431a4.5 4.5 0 1 1 8.398-3.235l17.557 45.575c16.44-7.813 27.836-24.577 27.836-43.957M26.442 99.838l15.872 41.203c.373.556.61 1.183.709 1.838l1.033 2.682a4.5 4.5 0 0 1-2.514 5.79l-23.974 9.686c-11.061 4.468-23.874 2.128-32.641-5.965a4.5 4.5 0 1 1 6.105-6.613c6.221 5.744 15.314 7.403 23.165 4.233l18.344-7.411c-22.776-12.678-48.489-19.383-74.585-19.383h-55.281l7.074 18.363h14.807a4.5 4.5 0 1 1 0 9h-37.011a4.5 4.5 0 0 1 0-9h12.559l-15.462-40.138a57.437 57.437 0 0 1-15.601 2.163c-31.785 0-57.644-25.858-57.644-57.642C-188.603 16.858-162.744-9-130.959-9c31.784 0 57.643 25.858 57.643 57.644 0 23.203-13.789 43.234-33.596 52.367l6.12 15.887h58.748c24.953 0 49.581 5.783 71.881 16.789l-11.69-30.344A57.393 57.393 0 0 1 0 106.286c-31.785 0-57.644-25.858-57.644-57.642C-57.644 16.858-31.785-9 0-9c31.784 0 57.643 25.858 57.643 57.644 0 22.256-12.687 41.592-31.201 51.194"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeDashoffset: 0,
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 4,
+          fill: "#000",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(3.30882 0 0 -3.30882 925.86 767.899)"
+      />
+      <path
+        d="M0 0a4.5 4.5 0 0 1-4.5-4.5c0-15.484-12.597-28.082-28.081-28.082S-60.663-19.984-60.663-4.5a4.5 4.5 0 0 1-9 0c0-20.447 16.635-37.082 37.082-37.082C-12.135-41.582 4.5-24.947 4.5-4.5A4.5 4.5 0 0 1 0 0"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeDashoffset: 0,
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 4,
+          fill: "#000",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(3.30882 0 0 -3.30882 1033.665 592.056)"
+      />
+      <path
+        d="M0 0a4.5 4.5 0 0 1-9 0c0-20.447 16.635-37.082 37.082-37.082 20.446 0 37.081 16.635 37.081 37.082a4.5 4.5 0 0 1-9 0c0-15.484-12.597-28.082-28.081-28.082C12.597-28.082 0-15.484 0 0"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeDashoffset: 0,
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 4,
+          fill: "#000",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(3.30882 0 0 -3.30882 399.622 606.946)"
+      />
+    </g>
+  </svg>
+);
+export default Logo;
